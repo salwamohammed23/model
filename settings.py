f873d1d457c45a84020f8dc96e48dc038e07d253
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2024-03-26 22:27:49 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -34,13 +34,13 @@ VIDEOS_DICT = {
 }
 
 # ML Model config
-MODEL_DIR = ROOT / 'weights'
-DETECTION_MODEL = MODEL_DIR / 'yolov8n.pt'
+
+DETECTION_MODEL = 'model_- 25 march 2024 2_53.pt'
 # In case of your custome model comment out the line above and
 # Place your custom model pt file name at the line below 
 # DETECTION_MODEL = MODEL_DIR / 'my_detection_model.pt'
 
-SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
+#SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
 
 # Webcam
 WEBCAM_PATH = 0
