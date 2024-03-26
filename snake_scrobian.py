0e8061ab39f1efb86e11b8a9ed9ed9b5cb83e323
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2024-03-26 23:53:35 +0200

    Update snake_scrobian.py

--- snake_scrobian.py
+++ snake_scrobian.py
@@ -4,6 +4,24 @@ import PIL
 import streamlit as st
 from ultralytics import YOLO
 
+
+# Get the absolute path of the current file
+FILE = Path(__file__).resolve()
+# Get the parent directory of the current file
+ROOT = FILE.parent
+# Add the root path to the sys.path list if it is not already there
+if ROOT not in sys.path:
+    sys.path.append(str(ROOT))
+# Get the relative path of the root directory with respect to the current working directory
+ROOT = ROOT.relative_to(Path.cwd())
+
+
+# ML Model config
+MODEL_DIR = ROOT / 'm'
+DETECTION_MODEL = MODEL_DIR / 'model_- 25 march 2024 2_53.ptt'
+
+
+
 # Replace the relative path to your weight file
 model_path = 'model_- 25 march 2024 2_53.pt'
 
