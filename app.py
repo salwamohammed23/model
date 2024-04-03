efec77986c6f0e9195f44bcd12c70b7032a1b8b4
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2024-04-03 04:26:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan  7 20:45:26 2021
+import threading
 
-@author: malraharsh
-"""
 import cv2
 import streamlit as st
+from matplotlib import pyplot as plt
 
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
+from streamlit_webrtc import webrtc_streamer
 
-while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+lock = threading.Lock()
+img_container = {"img": None}
+
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    with lock:
+        img_container["img"] = img
+
+    return frame
+
+
+ctx = webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+
+fig_place = st.empty()
+fig, ax = plt.subplots(1, 1)
+
+while ctx.state.playing:
+    with lock:
+        img = img_container["img"]
+    if img is None:
+        continue
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ax.cla()
+    ax.hist(gray.ravel(), 256, [0, 256])
+    fig_place.pyplot(fig)
