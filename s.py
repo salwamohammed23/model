adc602eefff4928513ec47c9463871883ccd9750
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2024-04-26 06:49:54 +0300

    Update s.py

--- s.py
+++ s.py
@@ -13,24 +13,27 @@ def predict_with_yolov8(img_bytes):
     # Run YOLOv8 segmentation on the image
     results = model.predict(pil_image, imgsz=600, conf=0.3, iou=0.5)
 
-    # Get the path of the new image saved by YOLOv8
-    # Assuming inference[0] is the Results object
-    res_plotted = results[0].plot()[:, :, ::-1]
-    pred = results[0].boxes.cls
-    
-    return res_plotted, pred
+    return results
 
 # Main code
 st.title("Skin Cancer Detection")
 
 # User image upload
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-out_img, out_name = predict_with_yolov8(uploaded_file)
-st.image(out_img, use_column_width=True, caption="Image")
 
-if out_name.numel() == 0:
-    st.markdown("**No Skin Cancer Detected.**")
-else:
-     st.markdown("**Skin Cancer Detected!**")
-# Perform prediction on uploaded image
+if uploaded_file is not None:
+    # Perform prediction on uploaded image
+    results = predict_with_yolov8(uploaded_file)
 
+    # Get the path of the new image saved by YOLOv8
+    # Assuming inference[0] is the Results object
+    res_plotted = results.plot()[:, :, ::-1]
+    pred = results.boxes.cls
+    
+    # Display image and results
+    st.image(res_plotted, use_column_width=True, caption="Image")
+
+    if pred.numel() == 0:
+        st.markdown("**No Skin Cancer Detected.**")
+    else:
+         st.markdown("**Skin Cancer Detected!**")
