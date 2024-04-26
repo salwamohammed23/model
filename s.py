f10b0473a6ce13dd5d6e48251e9aa3570ea5dbdc
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2024-04-26 06:53:00 +0300

    Update s.py

--- s.py
+++ s.py
@@ -1,39 +1,55 @@
-from ultralytics import YOLO
-import streamlit as st
-from PIL import Image
+from ultralytics import YOLO 
+import streamlit as st 
+from PIL import Image 
+import numpy as np
 
-# Function to predict with YOLOv8
-def predict_with_yolov8(img_bytes):
-    # Load the YOLOv8 model
-    model = YOLO('skin_can.pt')
+import cv2 import os import requests
 
-    # Convert the image bytes to PIL image
-    pil_image = Image.open(img_bytes)
+st.title("skin_canser") def predict_with_yolov8(img_bytes): # Load the YOLOv8 model model = YOLO('skin_can.pt')
 
-    # Run YOLOv8 segmentation on the image
-    results = model.predict(pil_image, imgsz=600, conf=0.3, iou=0.5)
+# Convert the image bytes to PIL image
+pil_image = Image.open(img_bytes)
 
-    return results
+# Run YOLOv8 segmentation on the image
+results = model.predict(pil_image, imgsz=600,conf=0.3, iou=0.5)
+# Get the path of the new image saved by YOLOv8
+# Assuming inference[0] is the Results object
+res_plotted = results[0].plot()[:, :, ::-1]
+pred= results[0].boxes.cls
 
-# Main code
-st.title("Skin Cancer Detection")
+return res_plotted,pred
 
-# User image upload
+# Convert the image bytes to PIL image
+pil_image = Image.open(img_bytes)
+
+# Run YOLOv8 segmentation on the image
+results = model.predict(pil_image, imgsz=600,conf=0.3, iou=0.5)
+# Get the path of the new image saved by YOLOv8
+# Assuming inference[0] is the Results object
+res_plotted = results[0].plot()[:, :, ::-1]
+pred= results[0].boxes.cls
+
+return res_plotted,pred
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-if uploaded_file is not None:
-    # Perform prediction on uploaded image
-    results = predict_with_yolov8(uploaded_file)
+Predict with YOLOv8 and Openai API
+if uploaded_file is not None: tab1, tab2 = st.tabs(["Skin_canser Detection", "webcam"]) out_img, out_name = predict_with_yolov8(uploaded_file)
 
-    # Get the path of the new image saved by YOLOv8
-    # Assuming inference[0] is the Results object
-    res_plotted = results.plot()[:, :, ::-1]
-    pred = results.boxes.cls
-    
-    # Display image and results
-    st.image(res_plotted, use_column_width=True, caption="Image")
+with tab1:
+        st.image(out_img, use_column_width=True,caption="Image")
+        
+        if out_name.numel()==0:
+            st.markdown("**No Smoke Detected.**")
+          
+            
+        else:
+            st.markdown("**Smoke Detected!**")
 
-    if pred.numel() == 0:
-        st.markdown("**No Skin Cancer Detected.**")
-    else:
-         st.markdown("**Skin Cancer Detected!**")
+        with tab2:    
+            st.title("webcam")
+            with st.expander("Start Camera"):
+                camera_image=st.camera_input("camera")
+            if camera_image:
+                img=Image.open(camera_image)
+                gray_scale=predict_with_yolov8(img)
+                st.image(gray_scale)
