21a707cedd8d9bb2d41a20f4b8a302b58101d665
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2024-04-26 06:55:51 +0300

    Update s.py

--- s.py
+++ s.py
@@ -7,31 +7,24 @@ import cv2
 import os 
 import requests
 
-st.title("skin_canser") def predict_with_yolov8(img_bytes): # Load the YOLOv8 model model = YOLO('skin_can.pt')
+st.title("skin_canser") 
+def predict_with_yolov8(img_bytes): 
+        # Load the YOLOv8 model 
+    model = YOLO('skin_can.pt')
 
 # Convert the image bytes to PIL image
-pil_image = Image.open(img_bytes)
+    pil_image = Image.open(img_bytes)
 
 # Run YOLOv8 segmentation on the image
-results = model.predict(pil_image, imgsz=600,conf=0.3, iou=0.5)
+    results = model.predict(pil_image, imgsz=600,conf=0.3, iou=0.5)
 # Get the path of the new image saved by YOLOv8
 # Assuming inference[0] is the Results object
-res_plotted = results[0].plot()[:, :, ::-1]
-pred= results[0].boxes.cls
+    res_plotted = results[0].plot()[:, :, ::-1]
+    pred= results[0].boxes.cls
 
-return res_plotted,pred
+    return res_plotted,pred
 
-# Convert the image bytes to PIL image
-pil_image = Image.open(img_bytes)
 
-# Run YOLOv8 segmentation on the image
-results = model.predict(pil_image, imgsz=600,conf=0.3, iou=0.5)
-# Get the path of the new image saved by YOLOv8
-# Assuming inference[0] is the Results object
-res_plotted = results[0].plot()[:, :, ::-1]
-pred= results[0].boxes.cls
-
-return res_plotted,pred
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 Predict with YOLOv8 and Openai API
